eab1faf62519c6f4bf4f9d001bc78d41ecc1740c
axel-glez <ricardoaxelabnalgonzalez@gmail.com>
2026-09-22 22:38:59 -0500

    Prettier error.

--- my-app/src/types/promotion.ts
+++ my-app/src/types/promotion.ts
@@ -12,7 +12,7 @@ export const promotionSchema = z.object({
       if (
         !value ||
         /^assets\/[a-zA-Z0-9_-]+\.(png|jpe?g|webp)$/.test(value) ||
-      /^\/api\/v1\/archivos\/[0-9a-f-]{36}$/.test(value)
+        /^\/api\/v1\/archivos\/[0-9a-f-]{36}$/.test(value)
       )
         return true;
       try {
